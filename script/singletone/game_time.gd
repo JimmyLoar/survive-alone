7e4c04fd1a76ebe4a90586eb8310629560faf8ce
JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
2024-10-03 19:03:21 +0300

    update game time: infinity time

--- script/singletone/game_time.gd
+++ script/singletone/game_time.gd
@@ -14,17 +14,28 @@ const _DATA =  {
 		"minut": 23,
 	}
 
-var _time: int = 0 
+var _time: int = 0  # 1 real second = 6 game minut
 var _day: int = 0 
 
 var _reaming_add_time: int = 0
-var _multiper: float = 0
+var _multiper: int = 1
+var _infinite: bool = true:
+	set(value):
+		_infinite = value
+		set_physics_process(value)
 
 
 func _init() -> void:
-	set_physics_process(false)
+	#set_physics_process(false)
 	_time = (_DATA.hour * 60 + _DATA.minut) * TIME_MULTYPER
 	_day = _DATA.month * 30 + _DATA.day
+
+
+func get_minut() -> int: return fmod(_time / TIME_MULTYPER, 60)
+func get_hour()  -> int: return floori(_time / TIME_MULTYPER / 60.0)
+func get_day()   -> int: return fmod(_day, 30.0) 
+func get_month() -> int: return ceil(fmod(_day, 360.0) / 30)
+func get_year()  -> int: return _DATA.year + floori(_day / 360.0)
 
 
 func get_date() -> Dictionary:
@@ -36,16 +47,19 @@ func get_date() -> Dictionary:
 		"minut": str(get_minut()).lpad(2, "0"),
 	}
 
-func get_minut() -> int: return fmod(_time / TIME_MULTYPER, 60)
-func get_hour()  -> int: return floori(_time / TIME_MULTYPER / 60.0)
-func get_day()   -> int: return fmod(_day, 30.0) 
-func get_month() -> int: return ceil(fmod(_day, 360.0) / 30)
-func get_year()  -> int: return _DATA.year + floori(_day / 360.0)
+
+func start():
+	_infinite = true
+	_multiper = 1
 
 
-func add_action_time(action_time: int = 0, multiper: float = 1.0):
+func stop():
+	_infinite = false
+
+
+func add_action_time(action_minut: int = 0, multiper: int = 1):
 	_multiper = clampi(multiper, 0, 1000)
-	_reaming_add_time += action_time * _multiper
+	_reaming_add_time += action_minut * _multiper * TIME_MULTYPER
 	set_physics_process(true)
 
 
@@ -54,19 +68,27 @@ func clear_action_time():
 	_multiper = 1.0
 
 
-func timeskip(time_in_minut: int) -> void:
-	pass
+func timeskip(minut: int) -> void:
+	_time_step(minut)
 
 
 func _physics_process(_delta: float) -> void:
-	if _reaming_add_time <= 0: 
-		set_physics_process(false)
-		return 
+	if _infinite: 
+		_time_step(_multiper)
+		return
 	
-	var time_addational = ceili(_multiper)
-	_reaming_add_time -= time_addational
-	_time += time_addational
-	emit_signal("time_update", time_addational)
+	elif _reaming_add_time > 0:
+		_reaming_add_time -= _multiper
+		_time_step(_multiper)
+		return
+	
+	set_physics_process(false)
+	return
+
+
+func _time_step(delta: int):
+	_time += delta
+	emit_signal("time_update", delta)
 	if _time >= MINUT_IN_DAY:
 		_day += floori(_time / MINUT_IN_DAY)
 		_time -= snapped(_time, MINUT_IN_DAY)
