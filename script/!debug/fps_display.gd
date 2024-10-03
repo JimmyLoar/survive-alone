ea3bfc82652ba9c8bd52161248823e4fe5242f72
JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
2024-10-03 12:01:40 +0300

    update fps display: transfer init from singleton in bootstrap

--- script/!debug/fps_display.gd
+++ script/!debug/fps_display.gd
@@ -1,9 +1,13 @@
+class_name FpsDisplay
 extends CanvasLayer
 
 var label := Label.new()
 func _ready() -> void:
+	name = "FpsDisplay"
+	label.position.x = 20
 	layer = 100
 	add_child(label)
+
 
 func _process(delta: float) -> void:
 	label.text = "FPS: %s" % Engine.get_frames_per_second()
