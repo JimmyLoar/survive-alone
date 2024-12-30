af170382e0c46434faaa1422a7174bfd93a457fe
JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
2024-12-31 02:25:46 +0300

    update PropertyGenerater: add methods to create properties; rename property "_name"

--- common/property_generater.gd
+++ common/property_generater.gd
@@ -6,47 +6,41 @@ func _init() -> void:
 	self.free()
 
 
-static func take_int(_name: String, range_values := [], _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+static func take_int(property_name: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
 	var result = {
-		"name": _name,
+		"name": property_name,
 		"type": TYPE_INT,
 		"usage": _usage,
 	}
-	if not range_values.is_empty():
-		result["hint"] = PROPERTY_HINT_RANGE
-		result["hint_string"] = "%s,".repeat(range_values.size()) % range_values
 	return result 
 
 
-static func take_float(_name: String, range_values := [], _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
-	var result := take_int(_name, range_values, _usage)
+static func take_float(property_name: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	var result := take_int(property_name, _usage)
 	result.type = TYPE_FLOAT
 	return result 
 
 
-static func take_resource(_name: String, _class_name: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+static func take_range_int(property_name: String, min_value: int, max_value: int, step: int = 1, overside: String = '', _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	var dict := take_int(property_name, _usage)
+	dict.merge({
+		"hint" = PROPERTY_HINT_RANGE,
+		"hint_string" = "%d,%d,%d,%s" % [min_value, max_value, step, overside]
+	})
+	return dict
+
+
+static func convent_to_range(property_dict: Dictionary, min_value: float, max_value: float, step: float = 1.0, overside: String = '') -> Dictionary:
+	property_dict.merge({
+		"hint" = PROPERTY_HINT_RANGE,
+		"hint_string" = "%s,%s,%s,%s" % [min_value, max_value, step, overside]
+	}, true)
+	return property_dict
+
+
+static func take_string(property_name: String, use_multiline := false, read_only := false) -> Dictionary:
 	return {
-		"name": _name,
-		"type": TYPE_OBJECT,
-		"hint": PROPERTY_HINT_RESOURCE_TYPE,
-		"hint_string": _class_name,
-		"usage": _usage,
-	}
-
-
-static func take_simple_array(_name: String, _subclass_name: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
-	return {
-		"name": _name,
-		"type": TYPE_ARRAY,
-		"hint": PROPERTY_HINT_ARRAY_TYPE,
-		"hint_string": _subclass_name,
-		"usage": _usage,
-	}
-
-
-static func take_string(_name: String, use_multiline := false, read_only := false) -> Dictionary:
-	return {
-		"name": _name,
+		"name": property_name,
 		"type": TYPE_STRING,
 		"hint": PROPERTY_HINT_MULTILINE_TEXT if use_multiline else PROPERTY_HINT_NONE,
 		"hint_string": "",
@@ -54,8 +48,8 @@ static func take_string(_name: String, use_multiline := false, read_only := fals
 	}
 
 
-static func take_enum_string(_name: String, enum_string: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
-	var _prop := take_string(_name)
+static func take_enum_string(property_name: String, enum_string: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	var _prop := take_string(property_name)
 	_prop.merge({
 		hint = PROPERTY_HINT_ENUM_SUGGESTION,
 		hint_string = enum_string,
@@ -64,11 +58,48 @@ static func take_enum_string(_name: String, enum_string: String, _usage: int = P
 	return _prop
 
 
-static func take_enum_int(_name: String, enum_string: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
-	var _prop = take_int(_name)
+static func take_enum_int(property_name: String, enum_string: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	var _prop = take_int(property_name)
 	_prop.merge({
 		hint = PROPERTY_HINT_ENUM,
 		hint_string = enum_string,
 		usage = _usage,
 	}, true)
 	return _prop
+
+
+static func take_simple_array(property_name: String, elem_type: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	return {
+		"name": property_name,
+		"type": TYPE_ARRAY,
+		"hint": PROPERTY_HINT_ARRAY_TYPE,
+		"hint_string": elem_type,
+		"usage": _usage,
+	}
+
+static func take_two_dimensional_array(property_name: String, elem_type: int = TYPE_INT, elem_hint: int = PROPERTY_HINT_NONE, elem_hint_string: String = '', _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	return {
+		"name": property_name,
+		"type": TYPE_ARRAY,
+		"hint": PROPERTY_HINT_TYPE_STRING,
+		"hint_string": "%d:%d/%d:%s" % [TYPE_ARRAY, elem_type, elem_hint, elem_hint_string],
+		"usage": _usage,
+	}
+
+static func take_free_dimensional_array(property_name: String, elem_type: int = TYPE_INT, elem_hint: int = PROPERTY_HINT_NONE, elem_hint_string: String = '', _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	return {
+		"name": property_name,
+		"type": TYPE_ARRAY,
+		"hint": PROPERTY_HINT_TYPE_STRING,
+		"hint_string": "%d:%d:%d/%d:%s" % [TYPE_ARRAY, TYPE_ARRAY, elem_type, elem_hint, elem_hint_string],
+		"usage": _usage,
+	}
+
+static func take_resource(property_name: String, _class_name: String, _usage: int = PROPERTY_USAGE_DEFAULT) -> Dictionary:
+	return {
+		"name": property_name,
+		"type": TYPE_OBJECT,
+		"hint": PROPERTY_HINT_RESOURCE_TYPE,
+		"hint_string": _class_name,
+		"usage": _usage,
+	}
--- feature/!world/structure/search_drop.gd
+++ feature/!world/structure/search_drop.gd
@@ -34,9 +34,15 @@ func _get_property_list() -> Array[Dictionary]:
 func _get_item_properties(index: int):
 	var properties: Array[Dictionary] = []
 	properties.append(PropertyGenerater.take_resource("item_%02d/data" % [index + 1], "ItemData"))
-	properties.append(PropertyGenerater.take_float("item_%02d/reward_count" % [index + 1], [1, 3, 0.01, "or_greater"]))
-	properties.append(PropertyGenerater.take_float("item_%02d/chance_base" % [index + 1], [0, 1, 0.01, "or_greater"]))
-	properties.append(PropertyGenerater.take_float("item_%02d/reduction_rate" % [index + 1], [1, 2, 0.001, "or_greater"]))
+	
+	var _property := PropertyGenerater.take_float("item_%02d/reward_count" % [index + 1])
+	properties.append(PropertyGenerater.convent_to_range(_property, 1, 3, 0.01, "or_greater"))
+	
+	_property = PropertyGenerater.take_float("item_%02d/chance_base" % [index + 1])
+	properties.append(PropertyGenerater.convent_to_range(_property, 0, 1, 0.01, "or_greater"))
+	
+	_property = PropertyGenerater.take_float("item_%02d/reduction_rate" % [index + 1])
+	properties.append(PropertyGenerater.convent_to_range(_property, 1, 2, 0.001, "or_greater"))
 	return properties
 
 
