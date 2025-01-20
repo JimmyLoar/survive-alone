32a02fad744a50453e559cf6bd72d94c06039049
Egorich0270 <egorich0270@gmail.com>
2025-01-20 17:51:31 +0300

    add: ui

---
+++ feature/rest/rest.gd
@@ -0,0 +1,14 @@
+extends MarginContainer
+
+
+
+func _on_button_accept_rest_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_accept_cancel_pressed():
+	close()
+
+
+func close():
+	pass
