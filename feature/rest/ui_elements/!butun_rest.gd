79571294cf68df4b985f5028ac53ede8ccc7c41f
Egorich0270 <egorich0270@gmail.com>
2025-01-31 23:36:12 +0300

    add: rest button

---
+++ feature/rest/ui_elements/!butun_rest.gd
@@ -0,0 +1,8 @@
+extends Button
+
+@onready var rest_state: RestScreenState = Injector.inject(RestScreenState, self)
+# Called when the node enters the scene tree for the first time.
+
+
+func _on_pressed():
+	rest_state.open()
