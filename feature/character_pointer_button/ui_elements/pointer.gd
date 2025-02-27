95579278ae2a8a35cc42a719dffeda93dfd32e7b
Egorich0270 <egorich0270@gmail.com>
2025-02-27 21:36:53 +0300

    autoref

--- feature/character_pointer_button/ui_elements/pointer.gd
+++ feature/character_pointer_button/ui_elements/pointer.gd
@@ -5,20 +5,25 @@ var dir_vector: Vector2 = Vector2.ZERO
 @onready var character_state: CharacterState = Injector.inject(CharacterState, self)
 @onready var camera_state: MainCameraState = Injector.inject(MainCameraState, self)
 
+
 func _ready():
 	visible = false
 	character_state.player_enter_on_screen.connect(enter_screen)
 	character_state.player_exited_from_screen.connect(exit_screen)
 
+
 func rotate_arrow():
-	$"Sprite-0003-export".rotation = (dir_vector).angle() - PI/2
-	
+	$"Sprite-0003-export".rotation = (dir_vector).angle() - PI / 2
+
+
 func _process(delta):
 	rotate_arrow()
+
 
 func enter_screen():
 	visible = false
 	set_process(false)
+
 
 func exit_screen():
 	visible = true
