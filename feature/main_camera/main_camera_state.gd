85ae35660a355155e4ae135506a770b39fcab003
JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
2025-03-03 22:49:44 +0300

    Squashed commit of the following:
    commit 6c63ad818512bbf44e107c40b282ee4b5af5504a
    Merge: 7857a3f5 787959f0
    Author: JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
    Date:   Mon Mar 3 22:39:42 2025 +0300
    
        Merge pull request #30 from JimmyLoar/jimmyloar_dev
    
        transfer to Godot Engine 4.4
    
    commit 7857a3f5156348549da74b4a42e77d406cd5f98f
    Merge: 6556a0b1 6b95e743
    Author: JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
    Date:   Mon Mar 3 22:19:12 2025 +0300
    
        Merge pull request #29 from JimmyLoar/Egor_dev
    
        Egor dev
    
    commit 6b95e743de5d3052a96e9bf0e6380a143b8fbda8
    Merge: 95579278 6556a0b1
    Author: JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
    Date:   Mon Mar 3 22:18:57 2025 +0300
    
        Merge branch 'dev' into Egor_dev
    
    commit 6556a0b15ec095c6e87ba8804c282cb2cd02c835
    Merge: c0fef3c1 20cc5f5c
    Author: JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
    Date:   Mon Mar 3 22:16:06 2025 +0300
    
        Merge pull request #28 from JimmyLoar/jimmyloar_dev
    
        new action system
    
    commit 95579278ae2a8a35cc42a719dffeda93dfd32e7b
    Author: Egorich0270 <egorich0270@gmail.com>
    Date:   Thu Feb 27 21:36:53 2025 +0300
    
        autoref
    
    commit d8f759f0d4d8ef8dca072c71cd7eacb4fcd77550
    Author: Egorich0270 <egorich0270@gmail.com>
    Date:   Thu Feb 27 21:36:21 2025 +0300
    
        add: feature character pointer
    
    commit c0fef3c14ec9ebe598ac20cdf15da7164d477127
    Merge: 28961a86 fa5af55c
    Author: JimmyLoar <70943502+JimmyLoar@users.noreply.github.com>
    Date:   Thu Feb 20 10:49:52 2025 +0300
    
        Merge pull request #27 from JimmyLoar/jimmyloar_dev
    
        BiomeSearchEvents
    
    commit 3f7122fa93ab834e0f5fafe3f6ea115c75c2e2aa
    Author: Egorich0270 <egorich0270@gmail.com>
    Date:   Wed Feb 12 23:33:34 2025 +0300
    
        add: camera folow mode and static pointer
    
    commit 3e2d8c8d05d1bf100f1f58e806f1c09c008e0830
    Merge: 61e170e3 5a25ea21
    Author: Egorich0270 <egorich0270@gmail.com>
    Date:   Sat Feb 8 11:51:55 2025 +0300
    
        Merge branch 'dev' into Egor_dev
    
    commit 61e170e39830e9d595e498f4248315fe43c43858
    Author: Egorich0270 <egorich0270@gmail.com>
    Date:   Sat Feb 1 16:37:10 2025 +0300
    
        fix
    
        Для новой системы времени.

--- feature/main_camera/main_camera_state.gd
+++ feature/main_camera/main_camera_state.gd
@@ -5,6 +5,9 @@ var _node: Camera2D
 func _init(node: Camera2D) -> void:
 	_node = node
 
+var global_position: Vector2:
+	get:
+		return _node.global_position
 
 var _viewport_rect: Rect2:
 	set(value):
@@ -30,6 +33,8 @@ class FreeMode:
 
 class TargetMode:
 	var target: Node2D
+	func _init(new_target: Node2D):
+		target = new_target
 
 var _mode: Variant = FreeMode.new() # FreeMode | TargetMode
 signal mode_changed(mode: Variant)
