0d30911b1377c63d3511548dd2b80d8560ef0885
Egorich0270 <egorich0270@gmail.com>
2025-03-25 22:09:13 +0300

    prepare to merge

---
+++ feature/Sound/SFX/sfx_player_state.gd
@@ -0,0 +1,20 @@
+class_name SfxPlayerState
+extends Injectable
+
+var _node
+
+func _init(node) -> void:
+	_node = node
+
+
+func play_sound_from_file(filepath) -> void:
+	if filepath is AudioStream:
+		pass
+	
+	if FileAccess.file_exists(filepath):
+		_node.play_sound(load(filepath))
+	else:
+		print(filepath, 'не найден')
+	
+func play_sound_from_stream(stream: AudioStream):
+	_node.play_sound(stream)
