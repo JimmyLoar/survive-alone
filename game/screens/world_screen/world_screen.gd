e5520cd285e4f15b764c07df710d5afa73ec75a7
Jimmyster <70943502+JimmyLoar@users.noreply.github.com>
2025-05-06 16:11:59 +0300

    update

--- game/screens/world_screen/world_screen.gd
+++ game/screens/world_screen/world_screen.gd
@@ -7,6 +7,7 @@ extends Node2D
 
 
 func _enter_tree() -> void:
+	Questify.condition_query_requested.connect(_on_condition_query_requested)
 	var save_db = Locator.get_service(SaveDb)
 	save_db.db_connect(save_db_path)
 
@@ -18,3 +19,24 @@ func _enter_tree() -> void:
 	Locator.initialize_service(CharacterRepository, [save_db])
 	
 	Locator.get_service(GameDb).db_connect(game_db_path)
+
+
+func _on_condition_query_requested(type: String, key: String, value: Variant, requester: QuestCondition):
+	if type != "world":
+		return
+	
+	var result = false
+	match key:
+		"structure_change": 
+			var location = Locator.get_service(CharacterLocationState)
+			if not location.current_location_changed.is_connected(_on_changed_location.bind(requester, value.split("/"))):
+				location.current_location_changed.connect(_on_changed_location.bind(requester, value.split("/")))
+	
+	requester.set_completed(result)
+
+
+func _on_changed_location(new_location, requester: QuestCondition, value: Array):
+	requester.set_completed(true)
+	Locator.get_service(CharacterLocationState).current_location_changed.disconnect(_on_changed_location.bind(requester, value))
+	Locator.get_service(CharacterState).stop_moving()
+	ActionMethods.new().callv(value[0], value.slice(1))
