2380cd451f2a57006bb33985ca9b1e772def445b
Jimmyster <70943502+JimmyLoar@users.noreply.github.com>
2025-05-23 18:24:05 +0300

    update event display

--- !tools/event_editer/source/sub_texture.gd
+++ !tools/event_editer/source/sub_texture.gd
@@ -1,4 +1,10 @@
 class_name EventSubTexture
-extends EventText
+extends EventNode
 
 @export var texture: String
+
+
+func get_active() -> bool:
+	if get_completed():
+		return false
+	return all_previous_nodes_completed() and not any_children_active()
