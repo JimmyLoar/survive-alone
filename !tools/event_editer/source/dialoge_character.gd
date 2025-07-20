f427eb61f4cb414c0bd08d583b6a1dc23a0e93fe
Jimmyster <70943502+JimmyLoar@users.noreply.github.com>
2025-07-20 21:16:44 +0300

    some fixes

--- !tools/event_editer/source/dialoge_character.gd
+++ !tools/event_editer/source/dialoge_character.gd
@@ -4,6 +4,6 @@ extends Resource
 @export var name: String
 
 @export var avatars: Dictionary[String, Texture2D] = {
-	defualt = null
+	defualt = preload("res://icon.svg"),
 }
 @export var name_modulate: Color = Color.WHITE
